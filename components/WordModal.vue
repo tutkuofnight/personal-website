9eaab74880207c508e8532e51af88b82bcf3af71
tutklon <mettheus0@gmail.com>
2022-12-04 11:57:57 +0300

    Fix WordModal responsive

--- components/WordModal.vue
+++ components/WordModal.vue
@@ -37,7 +37,7 @@ export default {
           p.mean(@click="seeMean = !seeMean") {{seeMean ? data.mean : "Click to see turkish mean"}}
           button(@click="randomWord") Give me another word
         footer
-          small English Practice! You will see random english words that i wrote every time to click 
+          small English Practice! You will see random english words that i wrote every time to click. Well, you just should guess
 </template>
 
 <style lang="scss" scoped>
@@ -51,14 +51,15 @@ export default {
   display: grid;
   place-items: center;
   .modal-border {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     display: flex;
     flex-direction: column;
     align-items: center;
     background: -webkit-linear-gradient(50deg , #e8398b, #ff0f33);
     width: 600px;
-    height: 300px;
+    height: 400px;
     border-radius: 12px;
-    padding: 4px;
+    padding: 2px;
     .modal {
       background: black;
       width: 100%;
@@ -66,6 +67,8 @@ export default {
       border-radius: 12px;
       color: white;
       position: relative;
+      padding: 5px;
+      padding-bottom: 10px;
       header {
         display: flex;
         justify-content: flex-end;
@@ -92,28 +95,33 @@ export default {
 
         h1 {
           margin-bottom: -20px;
+          font-size: 2.5rem;
         }
         .mean {
-          color: rgb(254, 114, 107);
+          color: rgb(255, 131, 125);
           cursor: pointer;
         }
         button {
           margin-bottom: -10px;
           background: none;
           padding: 10px 20px;
-          color: white;
+          color: whitesmoke;
           cursor: pointer;
-          border-top: 2px solid #e8398b;
-          border-bottom: 2px solid #e8398b;
+          border: 2px solid lightgray;
           transition: 300ms all;
+          border-radius: 10px;
           &:hover {
-            border-radius: 20px;
+            border-radius: 0px;
             border-color: #e8398b;
+          }
+          &:active {
+            color: lightgray;
           }
         }
       }
       footer {
-        padding: 5px 0px;
+        padding: 20px 50px;
+        text-align: center;
         width: 100%;
         display: flex;
         justify-content: center;
@@ -137,9 +145,6 @@ export default {
           p {
             margin-bottom: -40px;
           }
-        }
-        footer {
-          padding: 5px 20px;
         }
       }
     }

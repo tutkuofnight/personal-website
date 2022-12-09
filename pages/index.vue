6a783095115baa2f3f96bdf59148aa2ab33bf1f2
Tutku Uçan <tutkuucan369@gmail.com>
2022-12-09 04:09:07 +0300

    Update Route Links inside index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,35 @@
 <script>
 import { mapState } from 'vuex'
 export default {
-  name: 'IndexPage',
-  data() {
-    return {
-      flowerSection: false,
-      isLoading: {
-        content: true,
-        img: true,
-      },
-      preview: false,
-      wordModal: false
-    }
-  },
-  methods: {
-    imgLoad() {
-      this.isLoading.img = false
+    name: "IndexPage",
+    data() {
+        return {
+            flowerSection: false,
+            isLoading: {
+                content: true,
+                img: true,
+            },
+            preview: false,
+            wordModal: false
+        };
     },
-    closeModal(e){
-      this.wordModal = e
-    }
-  },
-  computed: {
-    ...mapState(['app', 'developer']),
-    divisionController() {
-      return this.app.theme.division == 4 ? 'flower-small' : 'flower'
+    methods: {
+        imgLoad() {
+            this.isLoading.img = false;
+        },
+        closeModal(e) {
+            this.wordModal = e;
+        }
     },
-  },
-  mounted() {
-    if (this.$route.query.flowerSection == 'true') this.flowerSection = true
-    console.log(`Developed by ${this.developer.fullname} 🌺`)
-    this.isLoading.content = false
-  },
+    computed: {
+        ...mapState(["app", "developer"]),
+    },
+    mounted() {
+        if (this.$route.query.flowerSection == "true")
+            this.flowerSection = true;
+        console.log(`Developed by ${this.developer.fullname} 🌺`);
+        this.isLoading.content = false;
+    },
 }
 </script>
 
@@ -58,43 +56,16 @@ export default {
             />
           </div>
           <div class="context">
-            <div class="flower-section" v-if="flowerSection">
-              <section>
-                <span class="linear-text-special special-event-text" v-if="app.theme.special">{{app.theme.special}}</span>
-                <h1 class="linear-text">{{ app.theme.flower.name }}</h1>
-                <small id="desc" class="linear-text">{{ app.theme.flower.desc }}</small>
-              </section>
-              <section class="img-list" v-if="app.theme.flower">
-                <img
-                  v-if="app.theme.flower.original"
-                  :class="divisionController"
-                  :src="`/img/divisions/${app.theme.name}/flower/${app.theme.flower.original}`"
-                />
-                <img
-                  v-if="app.theme.flower.icon_full"
-                  class="flower"
-                  :class="divisionController"
-                  :src="`/img/specials/${app.theme.name}/flower/${app.theme.flower.icon_full}`"
-                />
-              </section>
-              <a
-                class="redirect-link"
-                :href="app.theme.special ? app.theme.url : 'https://github.com/tutklon/personal-website#theme-'"
-                target="_blank"
-                rel="noopener noreferrer"
-                >{{app.theme.special ? "Click about what is today's special " : 'Click about flowers and theme'}}</a
-              >
-            </div>
+            <FlowerSection v-if="flowerSection"></FlowerSection>
             <template v-else>
               <section>
                 <h1 class="linear-text">{{ developer.fullname }}</h1>
                 <small class="linear-text">{{ developer.title }}</small>
               </section>
               <section>
-                <button class="direct-lnk disabled" to="#blog">/blog</button>
-                <button class="direct-lnk disabled" to="#cv">/cv</button>
+                <button class="direct-lnk disabled">/about</button>
                 <button class="direct-lnk" @click="wordModal = true">
-                  /words
+                  /word-game
                 </button>
               </section>
               <footer>
@@ -219,12 +190,6 @@ main {
   overflow: hidden;
   opacity: 0.8;
 }
-.flower-section {
-  padding-left: 40px;
-  #desc {
-    padding: 5px 10px;
-  }
-}
 @media screen and (max-width: 1400px) {
   .bg-img {
     width: fit-content;
@@ -257,10 +222,6 @@ main {
       }
       // background: rgba(#0000, $alpha: 0.9);
     }
-  }
-  .flower-section {
-    padding-left: 0;
-    text-align: center;
   }
 }
 .img-list {

33fd5829ef7af5ed50b1cb9d3f821e5cedeb919e
Tutku Uçan <mettheus0@gmail.com>
2023-09-29 03:20:15 +0300

    Update theme and website content

---
+++ components/Collection.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+.collection
+  h1 {{ title }}
+  p
+    slot(name="description")
+  .content 
+    slot(name="content")
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.collection {
+  margin-bottom: 40px;
+}
+</style>
